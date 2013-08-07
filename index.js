28cac98bb675a308cbf316f3b60951b17d9af425
Eldar Gabdullin <eldargab@gmail.com>
2013-08-07 16:41:53 +0400

    refactor exports

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var util = require('./lib/util')
+var App = require('./lib/app')
 
-exports = module.exports = require('./lib/container')
+exports = module.exports = function() {
+  return new App
+}
 
 exports.nsconcat = util.nsconcat
 
