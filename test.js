5cfaca20bd8dac85af6475fcf2b83de612375197
Eldar Gabdullin <eldargab@gmail.com>
2017-07-06 16:31:32 +0300

    plausible code generated

--- test.js
+++ test.js
@@ -14,14 +14,14 @@ app.def('abc', function(ab, c) {
   return ab + c
 })
 
-app.level('clevel', 'abc', ['c'])
+app.level('Abc', 'abc', ['c'])
 
-app.def('ccc', function*(clevel) {
-  let c = yield clevel('c')
-  let cc = yield clevel('ccc')
+app.def('ccc', function*(Abc) {
+  let c = yield Abc('c')
+  let cc = yield Abc('ccc')
   return c + cc
 })
 
-app.level('main', 'ccc')
+app.level('Main', 'ccc')
 
-console.log(app.toJS('main'))
+console.log(app.toJS('Main'))
